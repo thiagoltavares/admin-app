4e8c79d33356fa948793b8d238fe03935130c05c
Thiago Tavares <thiago.tavares@present-technologies.com>
2020-08-04 07:46:23 +0100

    initial commit

--- src/containers/templates/Dashboard/index.tsx
+++ src/containers/templates/Dashboard/index.tsx
@@ -17,7 +17,7 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
-import { Editor } from '@tinymce/tinymce-react';
+// import { Editor } from '@tinymce/tinymce-react';
 import CustomTable from "../../organism/CustomTable";
 
 import { router } from '../../../config/router';
