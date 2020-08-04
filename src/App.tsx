6c1f98fe9d93eb4d206785e57faef4dda558a34e
Thiago Tavares <thiago.tavares@present-technologies.com>
2020-08-04 21:14:19 +0100

    Update main component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,12 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import DashboardTemplate from "./containers/templates/Dashboard";
 import 'sweetalert2/dist/sweetalert2.css'
 
-const Root = () => <h1>Hello World</h1>
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Root} />
+        <Route path="/" exact component={DashboardTemplate} />
         <Route path="/dashboard" component={DashboardTemplate} />
       </Switch>
     </BrowserRouter>
